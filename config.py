1bc354199dd4aa711a4d1b9a8f4fbe3019c929c4
Mantix0 <89809041+Mantix0@users.noreply.github.com>
2024-08-02 15:38:41 +0300

    config update

--- config.py
+++ config.py
@@ -16,4 +16,12 @@ site_list = [
     #     'link': 'https://ria.ru/export/rss2/archive/index.xml',
     #     'content_selector': '#endless > div.endless__item.m-active > div > div > div > div.layout-article__over > div.layout-article__main > div > div:nth-child(1) > div.article__body.js-mediator-article.mia-analytics'
     # }
+    {
+        'name': 'Ведомости',
+        'logo': "https://yandex.ru/images/search?img_url=https%3A%2F%2Feng.magram.ru%2Fupload%2Fiblock%2F5c5%2F5c5c585d19b82ed8af335d1235bb571e.jpg&pos=9&rpt=simage&text=vedomosti%20logo",
+        'link': 'https://www.vedomosti.ru/rss/rubric/technology.xml',
+        'content_selector': '#app > div.layout > div.layout__wrapper.full-width > div.layout__container--transformed.layout__container > div.layout__page > div > div > div:nth-child(1) > div > div.article__container > div > div > div > div:nth-child(2)',
+        'additional_tags': ['Технологии'],
+        'delete_tags': ["article-boxes-list__item"],
+    },
 ]
